abf2c1674228cfdd5b67648eee332137c2c49cbd
baob <coder@onesandthrees.com>
2016-11-05 23:33:11 +0000

    bucket implemented with agent, part 2

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -5,6 +5,7 @@ defmodule KV.BucketTest do
     {:ok, bucket} = KV.Bucket.start_link
     assert KV.Bucket.get(bucket, "milk") == nil
 
-
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
   end
 end
